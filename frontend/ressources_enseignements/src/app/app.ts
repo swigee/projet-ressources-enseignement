c97d7a6f3ac9eafa8c2cee014fe1802a0e8fd66f
Enzo <saccone.enzo@etu.univ-lyon1.fr>
2025-12-01 11:23:58 +0100

    feat(maquette) - écran maquette pédagogique

--- frontend/ressources_enseignements/src/app/app.ts
+++ frontend/ressources_enseignements/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PedagogicalScheduleComponent } from "./components/pedagogical-schedule/pedagogical-schedule";
 import { RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, PedagogicalScheduleComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

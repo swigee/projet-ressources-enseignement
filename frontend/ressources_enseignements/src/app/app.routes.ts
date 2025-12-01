c87f7c55f66c92f55ef293c6030dda0d38e29439
Gleb <hlib.fedorchenko@etu.univ-lyon1.fr>
2025-12-01 09:26:31 +0100

    Feat: navbar fait (sauf partie dynamique)

--- frontend/ressources_enseignements/src/app/app.routes.ts
+++ frontend/ressources_enseignements/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component: Dashboard
+    },
+    {
+        path:'dashboard',
+        component: Dashboard
+    },
+    {
+        path:'syllabus',
+        component: Dashboard
+    },
+    {
+        path:'educational-model',
+        component: Dashboard
+    },
+    {
+        path:'service-sheet',
+        component: Dashboard
+    }
+];

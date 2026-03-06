1096ee63a29ae395e287ccd87664b449eab36bb5
selsebil <isira213213@gmail.com>
2026-03-06 11:09:54 +0100

    fix(auth/SecurityConfig): encodage des mots de passe des utilisateurs

--- backend/ressources_enseignements/src/main/java/sae/project/SecurityConfig.java
+++ backend/ressources_enseignements/src/main/java/sae/project/SecurityConfig.java
@@ -10,15 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
 
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-        System.out.println("admin123 -> " + encoder.encode("admin123"));
-        System.out.println("prof123 -> " + encoder.encode("prof123"));
-        System.out.println("student123 -> " + encoder.encode("student123"));
-        System.out.println("pass123 -> " + encoder.encode("pass123"));
-    }
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();

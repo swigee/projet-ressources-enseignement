80101412ec1e53fb2fc4b3bc36448257f7669789
Enzo <saccone.enzo@etu.univ-lyon1.fr>
2026-05-26 08:47:31 +0200

    fix(filtre):permet de filtrer par groupe

--- backend/ressources_enseignements/src/main/java/sae/project/repositories/EducationManagerRepository.java
+++ backend/ressources_enseignements/src/main/java/sae/project/repositories/EducationManagerRepository.java
@@ -12,12 +12,12 @@ import java.util.List;
 public interface EducationManagerRepository extends JpaRepository<Formation, Integer> {
     @Query("SELECT DISTINCT f FROM Formation f LEFT JOIN FETCH f.resourceList")
     List<Formation> findAllWithResources();
-//    @Query("SELECT DISTINCT f.className FROM Formation f " +
-//           "WHERE f.className IS NOT NULL " +
-//           "AND (:year IS NULL OR f.year = :year) " +
-//           "AND (:formation IS NULL OR f.name = :formation) " +
-//           "ORDER BY f.className")
-//    List<String> findDistinctClassNames(
-//            @Param("year") String year,
-//            @Param("formation") String formation);
+    @Query("SELECT DISTINCT f.className FROM Formation f " +
+           "WHERE f.className IS NOT NULL " +
+           "AND (:year IS NULL OR f.year = :year) " +
+           "AND (:formation IS NULL OR f.name = :formation) " +
+           "ORDER BY f.className")
+    List<String> findDistinctClassNames(
+            @Param("year") String year,
+            @Param("formation") String formation);
 }
